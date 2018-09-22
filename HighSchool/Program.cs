7bcc1a3f543816021d7e4f17a1180e23d9e03cc0
Scott Kuhl <scott@kuhl.ws>
2018-09-22 15:12:09 -0500

    Add project files.

---
+++ HighSchool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HighSchool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
