a2d7ad70930e5a69d549caac09a71bbd5f96add6
msunkist99 <msunkist99@gmail.com>
2019-03-05 17:56:55 -0600

    2019/03/05 - initial code

--- HighSchool/Program.cs
+++ HighSchool/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace HighSchool
 {
@@ -6,6 +8,27 @@ namespace HighSchool
     {
         public static void Main()
         {
+            List<Student> students = new List<Student>()
+            {
+                {new Student() {LastName = "Smith", FirstName = "Abby", Grade = 1, GPA = 3.25 } },
+                {new Student() {LastName = "Wallen", FirstName = "Alex", Grade = 2, GPA = 2.5 } },
+                {new Student() {LastName = "Jones", FirstName = "Lauren", Grade = 8, GPA = 2 } },
+                {new Student() {LastName = "Star", FirstName = "Drew", Grade = 9, GPA = 3.75 } },
+                {new Student() {LastName = "Jordan", FirstName = "Sara", Grade = 10, GPA = 4 } },
+                {new Student() {LastName = "Barnes", FirstName = "Cameron", Grade = 11, GPA = 3.5 } },
+                {new Student() {LastName = "Pointer", FirstName = "Linsey", Grade = 10, GPA = 3 } },
+                {new Student() {LastName = "Fletcher", FirstName = "Ashley", Grade = 9, GPA = 3.25 } },
+                {new Student() {LastName = "Williams", FirstName = "Jaden", Grade = 11, GPA = 2.75 } },
+                {new Student() {LastName = "Powers", FirstName = "Danielle", Grade = 13, GPA = 2.25 } }
+            };
+
+            List<Student> selectedStudents = Student.StarStudents(students);
+
+            foreach (var student in selectedStudents)
+            {
+                Console.WriteLine(student.FirstName);
+            }
+
             Console.ReadLine();
         }
     }
--- HighSchool/Student.cs
+++ HighSchool/Student.cs
@@ -5,8 +5,60 @@ namespace HighSchool
 {
     public class Student
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        private int _grade;
+        private double _gpa;
+
+        public int Grade
+        {
+            get { return _grade; }
+
+            set
+            {
+                if ((value < 1) || (value > 12))
+                {
+                    _grade = 0;
+                }
+                else
+	            {
+                    _grade = value;
+                };
+            }
+        }
+
+        public double GPA {
+            get { return _gpa; }
+
+            set
+            {
+                if ((value < 1) || (value > 4))
+                {
+                    _gpa = 0;
+                }
+                else
+	            {
+                    _gpa = value;
+                }
+            }
+        }
+
         public static List<Student> StarStudents(List<Student> students)
         {
+            var selected = from student in students
+                           where student.GPA >= 3 &&
+                                 student.Grade >= 9 &&
+                                 student.Grade <= 12
+                           orderby student.Grade descending,
+                                   student.GPA descending,
+                                   student.LastName ascending,
+                                   student.FirstName ascending
+                          select student;
+
+            List<Student> selectedStudents = selected.ToList();
+
+            return selectedStudents;
         }
     }
 }
